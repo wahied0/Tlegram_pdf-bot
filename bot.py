f788839a400bb70a0864f029ab2aa58569dabdec
wahied0 <151039037+wahied0@users.noreply.github.com>
2026-02-05 11:35:48 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,16 @@ import telebot
 from datetime import datetime
 import os
 
-
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("oken")      # الاسم زي ما هو في GitHub Secret
 PASSWORD = os.getenv("PASSWORD")
-PDF_FOLDER = "pdfs"  # المجلد اللي فيه ملفات PDF
-# ====================
+
+PDF_FOLDER = "pdfs"
 
 bot = telebot.TeleBot(TOKEN)
 print("Bot started... waiting for messages")
 
-# دالة تجيب مسار PDF اليوم
 def get_today_pdf():
-    today = datetime.now().day  # رقم اليوم من 1 لـ 31
+    today = datetime.now().day
     file_name = f"day{today}.pdf"
     file_path = os.path.join(PDF_FOLDER, file_name)
     if os.path.exists(file_path):
@@ -21,16 +19,12 @@ def get_today_pdf():
     else:
         return None
 
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    print(f"Received /start from {message.chat.id}")  # Debug
     bot.send_message(message.chat.id, "أهلاً 👋\nادخلي الباسورد يا هبتي 🔐")
-
 
 @bot.message_handler(func=lambda m: True)
 def check_password(message):
-    print(f"Received message: {message.text} from {message.chat.id}")  # Debug
     if message.text == PASSWORD:
         pdf_file = get_today_pdf()
         if pdf_file:
@@ -39,7 +33,4 @@ def check_password(message):
         else:
             bot.send_message(message.chat.id, "لا يوجد PDF لليوم 🤍")
     else:
-        bot.send_message(message.chat.id, "❌ الباسورد غلط")
-
-# تشغيل البوت
-bot.infinity_polling()
+        bot.send_message(message.chat.id, "❌ ا_
